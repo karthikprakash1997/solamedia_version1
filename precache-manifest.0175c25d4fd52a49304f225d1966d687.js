b4b64adbeaff39af5cf7a808467637102e8c384b
karthik-prakash <karthikprakash.s@aximsoft.com>
2020-07-30 19:59:21 +0530

    Updates

---
+++ precache-manifest.0175c25d4fd52a49304f225d1966d687.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a694443f33bd0c0fc1406e0de036c9",
+    "url": "/%7Bsolamedia%7D/index.html"
+  },
+  {
+    "revision": "43bad89dbd2db22f97ce",
+    "url": "/%7Bsolamedia%7D/static/css/2.2c32fbbd.chunk.css"
+  },
+  {
+    "revision": "ac5dc457ed1cbf3cffef",
+    "url": "/%7Bsolamedia%7D/static/css/main.33bdc947.chunk.css"
+  },
+  {
+    "revision": "43bad89dbd2db22f97ce",
+    "url": "/%7Bsolamedia%7D/static/js/2.dfd4ac43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7Bsolamedia%7D/static/js/2.dfd4ac43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5dc457ed1cbf3cffef",
+    "url": "/%7Bsolamedia%7D/static/js/main.d668f0d0.chunk.js"
+  },
+  {
+    "revision": "cd28036fafcfab5dced2",
+    "url": "/%7Bsolamedia%7D/static/js/runtime-main.24192163.js"
+  },
+  {
+    "revision": "f062eb5db530688aa47a70b935168bc1",
+    "url": "/%7Bsolamedia%7D/static/media/concert.f062eb5d.jpg"
+  },
+  {
+    "revision": "7c745f0bd1802300925cc4b5c07a05bc",
+    "url": "/%7Bsolamedia%7D/static/media/festive-img2.7c745f0b.png"
+  },
+  {
+    "revision": "44d56a0b2aaa8254418b96240370a063",
+    "url": "/%7Bsolamedia%7D/static/media/festivetextshape.44d56a0b.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/%7Bsolamedia%7D/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/%7Bsolamedia%7D/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/%7Bsolamedia%7D/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/%7Bsolamedia%7D/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/%7Bsolamedia%7D/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "720c77ee4af2a101b7a57a2d1605cc8f",
+    "url": "/%7Bsolamedia%7D/static/media/gallery-bg.720c77ee.png"
+  },
+  {
+    "revision": "3c85a08e830c433ee0559f759ed7d93c",
+    "url": "/%7Bsolamedia%7D/static/media/speaker-bg.3c85a08e.png"
+  },
+  {
+    "revision": "5af553da273b1abc45033fed7608b69a",
+    "url": "/%7Bsolamedia%7D/static/media/speaker-img1.5af553da.png"
+  },
+  {
+    "revision": "6111af0b11ac27ac803e3dce0ffcfb1e",
+    "url": "/%7Bsolamedia%7D/static/media/testimonial-img1.6111af0b.png"
+  }
+]);
