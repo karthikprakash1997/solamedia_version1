2b826016f579b63c59f4b9739336c0578d6a2d2a
karthik-prakash <karthikprakash.s@aximsoft.com>
2020-08-12 20:15:43 +0530

    toggle bar activated

--- src/header/header.js
+++ src/header/header.js
@@ -1,8 +1,136 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 
 function Header() {
+  const [drawer, toggleDrawer] = useState(false);
+
+  const anchor = () => (
+    <div>
+      <ul>
+        <li>
+          <Link
+            to="About"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="nav-link"
+            activeClass="nav-link"
+            onClick={() => toggleDrawer(false)}
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="Celeberities"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="nav-link"
+            activeClass="nav-link"
+            onClick={() => toggleDrawer(false)}
+          >
+            Celeberities
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="Events"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="nav-link"
+            activeClass="nav-link"
+            onClick={() => toggleDrawer(false)}
+          >
+            Events
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            to="Videos"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="nav-link"
+            activeClass="nav-link"
+            onClick={() => toggleDrawer(false)}
+          >
+            Videos
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            to="Gallery"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="nav-link"
+            activeClass="nav-link"
+            onClick={() => toggleDrawer(false)}
+          >
+            Gallery
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="Gallery"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="nav-link"
+            activeClass="nav-link"
+            onClick={() => toggleDrawer(false)}
+          >
+            PR News
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="example-destination"
+            spy={true}
+            smooth={true}
+            duration={900}
+            className="nav-link"
+            activeClass="nav-link"
+            onClick={() => toggleDrawer(false)}
+          >
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="Feedback"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="nav-link"
+            activeClass="nav-link"
+            onClick={() => toggleDrawer(false)}
+          >
+            Feedback
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="Contact"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="nav-link"
+            activeClass="nav-link"
+            onClick={() => toggleDrawer(false)}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
   return (
     <header className="header-part sticky">
       <nav className="navbar navbar-expand-lg">
@@ -15,9 +143,19 @@ function Header() {
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            onClick={() => {
+              toggleDrawer(true);
+            }}
           >
             <i className="fa fa-bars" aria-hidden="true"></i>
           </button>
+          <SwipeableDrawer
+            anchor="left"
+            open={drawer}
+            onClose={() => toggleDrawer(false)}
+          >
+            {drawer && anchor()}
+          </SwipeableDrawer>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
